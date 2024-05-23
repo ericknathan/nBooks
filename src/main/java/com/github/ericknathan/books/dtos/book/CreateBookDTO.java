416b76bd645151879d8ca047b8dc86bb8b8cdc08
ericknathan <erick.capito@hotmail.com>
2024-05-23 20:53:44 -0300

    fix: book bean validation

--- src/main/java/com/github/ericknathan/books/dtos/book/CreateBookDTO.java
+++ src/main/java/com/github/ericknathan/books/dtos/book/CreateBookDTO.java
@@ -1,29 +1,27 @@
 package com.github.ericknathan.books.dtos.book;
 
 import com.github.ericknathan.books.enums.BookGenre;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public record CreateBookDTO(
-        @NotBlank
+        @NotNull
         Long companyId,
         @NotBlank @Size(min = 2, max = 100)
         String title,
-        @NotBlank @Past
+        @NotNull @Past
         LocalDate publishedAt,
         @NotBlank @Size(min = 10, max = 500)
         String summary,
-        @NotBlank @Size(min = 1)
+        @NotNull @Positive
         Integer pages,
         @NotBlank @Size(min = 2, max = 50)
         String language,
-        @NotBlank @Size(min = 2, max = 50)
+        @NotNull
         BookGenre genre,
-        @NotBlank
+        @NotEmpty
         List<Long> authors
 ) {
 }
